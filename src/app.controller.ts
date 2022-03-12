80f43ed30177bc1973d7a321e91a8d1a3ebf484b
Lucca Ceresa Zimmermann <3362782-luccazii@users.noreply.gitlab.com>
2022-03-11 21:48:29 -0300

    Initial commit

---
+++ src/app.controller.ts
@@ -0,0 +1,24 @@
+import {Controller, Get, Param} from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello() {
+    return this.appService.getHello();
+  }
+
+  @Get('server-info/:host/:port')
+  getServerInfo(@Param() params) {
+    console.log(params.host);
+    return this.appService.getServerInfo(params.host, params.port);
+  }
+
+  @Get('servers')
+  getServerList() {
+    return this.appService.getServerList();
+  }
+
+}
---
+++ src/app.service.ts
@@ -0,0 +1,66 @@
+import {Injectable, Inject, CACHE_MANAGER} from '@nestjs/common';
+import { Cache } from 'cache-manager';
+
+const Gamedig = require('gamedig');
+
+@Injectable()
+export class AppService {
+
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
+
+  async getHello() {
+    await this.cacheManager.set('foo2', 'bar3', {ttl: 100000});
+
+    const res =  await this.cacheManager.get('foo2');
+    console.log(res); // logs "bar2"
+
+    return 'Hello World lol: ' + res;
+  }
+
+  async getServerInfo(ip, port) {
+    const cachingKey = ip + ':' + port
+
+    if(await this.cacheManager.get(cachingKey)){
+      console.log('cached data: ')
+      return this.cacheManager.get(cachingKey);
+    }
+
+    else {
+      console.log('getServerDataFromServer')
+      const freshServerData = await this.getServerDataFromServer(ip, port);
+      this.cacheManager.set(cachingKey, freshServerData, {ttl: 100000});
+
+      return freshServerData;
+    }
+
+  }
+
+
+  async getServerDataFromServer(ip, port) {
+    return await Gamedig.query({
+      type: 'csgo',
+      host: ip,
+      port: port,
+    })
+    .then(serverData => {
+      this.pushServerToList(serverData);
+      return serverData;
+    });
+  }
+
+
+  async pushServerToList(serverData){
+    let newAllServers = await this.cacheManager.get('all') || [];
+    newAllServers.push(serverData);
+
+    this.cacheManager.set('all', newAllServers, {ttl: 100000});
+
+    return newAllServers;
+  }
+
+
+  async getServerList(){
+    return this.cacheManager.get('all') || [];
+  }
+
+}
